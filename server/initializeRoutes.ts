e51133edf54a1a24b6c4c96cbf01501090ce1fb2
Marco Pestrin <pestrinmarco@gmail.com>
2021-03-17 21:06:40 +0100

    route 404

--- server/initializeRoutes.ts
+++ server/initializeRoutes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 const user = require('./routes/user');
 const auth = require('./routes/auth')
 const dev = require('./routes/dev');
@@ -7,4 +7,10 @@ export default function initializeRoutes(router: Router): void {
     router.use("/v1/user", user);
     router.use("/v1/auth", auth);
     router.use("/v1/dev", dev);
+
+    router.use((req: Request, res: Response) => {
+        res.status(404).json({
+            error: "page not found"
+        })
+    })
 }
