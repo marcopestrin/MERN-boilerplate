6734996f715b8817ffde92c8a018dfae8e2403ef
Marco Pestrin <pestrinmarco@gmail.com>
2021-04-12 22:03:00 +0200

    const file

--- const/index.ts
+++ const/index.ts
@@ -1,16 +1,16 @@
 export const isProduction = process.env.NODE_ENV === "production";
 export const port = process.env.PORT ? process.env.PORT : 8000;
 export const host = `http://localhost:${port}`;
-
-export const databaseName = "mern-boilerplate";
-export const databasePort = "27017";
-export const databaseServer = "localhost";
 export const applicationDomain = "pes-mern-boilerplate";
-export const expiresAccessToken =  900000; // millisecond
 
-export const usernameDatabase = process.env.USERNAME_DATABASE;
-export const passwordDatabase = process.env.PASSWORD_DATABASE;
-export const hostDatabase = process.env.HOST_DATABASE;
-export const nameDatabase = process.env.NAME_DATABASE;
+const databaseName = "mern-boilerplate";
+const databasePort = "27017";
+const databaseServer = "localhost";
+
+// export const usernameDatabase = process.env.USERNAME_DATABASE;
+// export const passwordDatabase = process.env.PASSWORD_DATABASE;
+// export const hostDatabase = process.env.HOST_DATABASE;
+// export const nameDatabase = process.env.NAME_DATABASE;
+
 export const urlDatabaseProduction = process.env.URL_DATABASE;
 export const urlDatabaseDevelopment = `mongodb://${databaseServer}:${databasePort}/${databaseName}`
