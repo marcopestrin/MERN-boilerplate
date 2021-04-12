1cee0dfae44a31735a7b01b7d5b0afe12c59c070
Marco Pestrin <pestrinmarco@gmail.com>
2021-04-12 21:38:43 +0200

    set localstorage in reducer

--- client/src/components/userTable/index.js
+++ client/src/components/userTable/index.js
@@ -13,36 +13,38 @@ const UserTable = ({
 
     return (
         <TableContainer>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Username</TableCell>
-                        <TableCell>Email</TableCell>
-                        <TableCell>Ruolo</TableCell>
-                        <TableCell>Attivo</TableCell>
-                        <TableCell>Data di registrazione</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    { users.map(({
-                        email,
-                        active,
-                        role,
-                        username,
-                        timeRegistration
-                    }) => {
-                        return (
-                            <TableRow>
-                                <TableCell>{ username }</TableCell>
-                                <TableCell>{ email }</TableCell>
-                                <TableCell>{ role === 1 ? "Admin" : "User" }</TableCell>
-                                <TableCell>{ active ? "si" : "no" }</TableCell>
-                                <TableCell>{ timeRegistration }</TableCell>
-                            </TableRow>
-                        )
-                    })}
-                </TableBody>
-            </Table>
+            { users && (
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Username</TableCell>
+                            <TableCell>Email</TableCell>
+                            <TableCell>Ruolo</TableCell>
+                            <TableCell>Attivo</TableCell>
+                            <TableCell>Data di registrazione</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        { users.map(({
+                            email,
+                            active,
+                            role,
+                            username,
+                            timeRegistration
+                        }) => {
+                            return (
+                                <TableRow>
+                                    <TableCell>{ username }</TableCell>
+                                    <TableCell>{ email }</TableCell>
+                                    <TableCell>{ role === 1 ? "Admin" : "User" }</TableCell>
+                                    <TableCell>{ active ? "si" : "no" }</TableCell>
+                                    <TableCell>{ timeRegistration }</TableCell>
+                                </TableRow>
+                            )
+                        })}
+                    </TableBody>
+                </Table>
+            )}
         </TableContainer>
     )
 }
