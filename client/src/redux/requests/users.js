a605fadfaa9d02d114a4f26ecf3f3463a9f2b80f
Marco Pestrin <pestrinmarco@gmail.com>
2021-05-15 10:58:00 -0500

    compliance in requests

--- client/src/redux/requests/users.js
+++ client/src/redux/requests/users.js
@@ -1,44 +1,17 @@
-import fetcher, { getEndpointList } from "./helpers";
+import { getEndpointList } from "./helpers";
 import http from "./index";
 
 export const getUsersList = async() => {
     const { user } = await getEndpointList();
-    const { data, error } = await fetcher({
-        url: user.getAllUsers,
-        method: "GET"
-    });
-    if (error) {
-        return {
-            success: false,
-            error
-        }
-    }
+    const result = await http.get(user.getAllUsers);
     return {
-        data,
+        data: result.data,
         success: true
-    };
+    }
 }
 
 export const editUser = async(payload) => {
     const { user } = await getEndpointList();
-    // const { data, error } = await fetcher({
-    //     url: user.update,
-    //     method: "POST",
-    //     params: {
-    //         id: payload.userId
-    //     },
-    //     body: payload
-    // });
-    // if (error) {
-    //     return {
-    //         success: false,
-    //         error
-    //     }
-    // }
-    // return {
-    //     data,
-    //     success: true
-    // };
     const data = await http.post(user.update, payload, {
         params: { id: payload.id }
     })
