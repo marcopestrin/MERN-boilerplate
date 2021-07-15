0a140e651e4b065f286bd8c08a1c7f3c73a911f3
Marco Pestrin <pestrinmarco@gmail.com>
2021-07-15 15:52:00 -0500

    backend log for debugging

--- server/initializeCors.ts
+++ server/initializeCors.ts
@@ -21,10 +21,13 @@ export default function initializeCors(app: Express): void {
 			console.log("origin", origin);
 			console.log("whitelist", whitelist);
 			if (!origin) {
+				console.log("NON consentito 1")
 				callback(null, true);
 			} else if (whitelist.indexOf(origin) !== -1) {
+				console.log("consentito 2")
 				callback(null, true);
 			} else {
+				console.log("NON consentito 3")
 				callback(new Error(`${origin} is not allowed by CORS`));
 			}
 		},
