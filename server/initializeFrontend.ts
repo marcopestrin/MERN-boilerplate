12ab4d3fbf24ccdf2ed3ec4142249224ef3e16af
Marco Pestrin <pestrinmarco@gmail.com>
2021-07-16 14:51:59 -0500

    bugfix express

--- server/initializeFrontend.ts
+++ server/initializeFrontend.ts
@@ -6,7 +6,7 @@ export default function initializeFrontend(app: Express): void {
     if(isProduction) {
         const rootPath = path.resolve("./");
         app.use(express.static('client/build'));
-        app.get('*', (req, res) => {
+        app.get('/*', (req, res) => {
           res.sendFile(path.resolve(rootPath, 'client/build', 'index.html'));
         });
     }
